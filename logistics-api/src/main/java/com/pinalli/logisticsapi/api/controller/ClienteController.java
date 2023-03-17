2f8888b744e31f7b67fd98da898adbd68479c8f8
pinalli <betopinali@gmail.com>
2023-03-17 10:02:14 -0300

    validation

--- logistics-api/src/main/java/com/pinalli/logisticsapi/api/controller/ClienteController.java
+++ logistics-api/src/main/java/com/pinalli/logisticsapi/api/controller/ClienteController.java
@@ -1,7 +1,19 @@
 package com.pinalli.logisticsapi.api.controller;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pinalli.logisticsapi.domain.model.Cliente;
@@ -11,14 +23,54 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
-		return clienteRepository.findByNome("João");
+		return clienteRepository.findAll();		
+	}
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {	
 		
+		return clienteRepository.findById(clienteId)
+				.map(cliente -> ResponseEntity.ok(cliente))//ou .map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		/*
+		 * Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		 * 
+		 * if(cliente.isPresent()) { return ResponseEntity.ok(cliente.get()); } return
+		 * ResponseEntity.notFound().build();
+		 */
+	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente>atualizar(@PathVariable Long clienteId,
+			@Valid @RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);	
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(clienteId);	
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- logistics-api/src/main/java/com/pinalli/logisticsapi/domain/model/Cliente.java
+++ logistics-api/src/main/java/com/pinalli/logisticsapi/domain/model/Cliente.java
@@ -5,6 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,9 +23,21 @@ public class Cliente {
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;	
+	private Long id;
+	
+	@NotBlank
+	@Size(max=60)
+	
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max=200)
 	private String email;
+	
+	@NotBlank
+	@Size(max=11)
+	@Size(max=200)
 	@Column(name="fone")
 	private String telefone;
 }
